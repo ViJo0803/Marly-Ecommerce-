961738e0c411efa60d43eb919993275de62f5c2a
ViJo0803 <84634814+ViJo0803@users.noreply.github.com>
2021-10-07 12:25:22 -0500

    Add files via upload

---
+++ slide.php
@@ -0,0 +1,27 @@
+<!-- Start WOWSlider.com BODY section -->
+<div id="wowslider-container1">
+<div class="ws_images"><ul>
+        <li><img src="data1/images/1.jpg" alt="" title="" id="wows1_0"/></li>
+        <li><img src="data1/images/2.jpg" alt="" title="" id="wows1_1"/></li>
+        <li><img src="data1/images/3.jpg" alt="" title="" id="wows1_2"/></li>
+        <li><img src="data1/images/4.jpg" alt="multipro_general" title="" id="wows1_3"/></li>
+        <li><img src="data1/images/5.jpg" alt="slider bootstrap" title="" id="wows1_4"/></a></li>
+        <li><img src="data1/images/6.jpg" alt="wd_40" title="" id="wows1_5"/></li>
+        <li><img src="data1/images/7.jpg" alt="wd_40" title="" id="wows1_6"/></li>
+        <li><img src="data1/images/8.jpg" alt="wd_40" title="" id="wows1_7"/></li>
+    </ul></div>
+    <div class="ws_bullets"><div>
+        <a href="#" title=""><span></span></a>
+        <a href="#" title=""><span></span></a>
+        <a href="#" title=""><span></span></a>
+        <a href="#" title=""><span></span></a>
+        <a href="#" title=""><span></span></a>
+         <a href="#" title=""><span></span></a>
+          <a href="#" title=""><span></span></a>       
+        <a href="#" title=""><span></span></a>
+    </div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.com">jquery image slider</a> by herry prasetyo</div>
+<div class="ws_shadow"></div>
+</div>  
+<script type="text/javascript" src="engine1/wowslider.js"></script>
+<script type="text/javascript" src="engine1/script.js"></script>
+<!-- End WOWSlider.com BODY section -->
